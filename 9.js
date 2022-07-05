e5ce2f334cd716e9097d9790b7f882dbdecaf8f3
Bryare <bryanmarevalo@gmail.com>
2022-07-05 10:52:00 -0500

    add challenge 9; is nummber palindrome

---
+++ 9.js
@@ -0,0 +1,14 @@
+var isPalindrome = function(x) {
+    if(x < 0) return false;
+    
+    let reversed = 0, y = x;
+    
+    while (y > 0){
+        
+        const lastDigit = y % 10;
+        reversed = (reversed * 10) + lastDigit;
+        y = (y /10) | 0;
+    }
+    
+    return x === reversed;
+};
